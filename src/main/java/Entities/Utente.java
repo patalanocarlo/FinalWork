609eb307f9ec93fad1fac9750f9bcfe18298b3f1
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-23 09:50:04 +0200

    Initial commit

---
+++ src/main/java/Entities/Utente.java
@@ -0,0 +1,14 @@
+package Entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Utente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/test/java/CarloPatalanoCapstoneProject/FinalWork/FinalWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package CarloPatalanoCapstoneProject.FinalWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
